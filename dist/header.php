578c2c4670bded460f64e703b1d3b4a271d839a1
Leandro Atallah <leandro.atallah@artedigitalinternet.com.br>
2018-12-13 05:16:00 -0200

    first commt

---
+++ dist/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Reescrevendo Esperança</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<header>
+		<div id="top-nav">
+			<div class="wrapper">
+				<div class="container-fluid">
+					<div class="row justify-content-between align-items-center">
+						<div class="offset-4 col-4 text-center">
+							<span>Uma dose de motivação para sua vida</span>
+						</div>
+						<div class="col-4  text-right">
+							<ul class="list-nav social-nav">
+								<li><a href="#"><img src="assets/img/icon_face.png" /></a></li>
+								<li><a href="#"><img src="assets/img/ico_insta.png" /></a></li>
+								<li><a href="#"><img src="assets/img/ico_twi.png" /></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<h1 id="branding">
+			<a href="#">
+				<img src="assets/img/logo.png" />
+			</a>
+		</h1>
+		<nav>
+			<div class="wrapper">
+				<div class="container-fluid">
+					<div class="row justify-content-between align-items-center">
+						<div class="col-auto">
+							<ul class="list-nav">
+								<li><a href="#">Inicio</a></li>
+								<li><a href="#">Blog</a></li>
+								<li><a href="#">Categorias</a></li>
+								<li><a href="#">Depoimentos</a></li>
+								<li><a href="#">Sobre nós</a></li>
+								<li><a href="#">Contato</a></li>
+							</ul>
+						</div>
+						<div class="col-auto">
+							<form>
+								<label>
+									<input type="text" name="search" placeholder="Pesquisar..." />
+									<input type="submit" value="" />
+								</label>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</header>
